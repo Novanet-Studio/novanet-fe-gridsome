bc9546027b2459796203b5d10176d21bc72a4d2e
Freddy Polania <fpolania@gmail.com>
2021-08-24 16:54:42 -0400

    Gridsome Sass template

--- gridsome.config.js
+++ gridsome.config.js
@@ -3,8 +3,26 @@
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
+const path = require('path')
+
+function addStyleResource(rule) {
+  rule
+    .use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [path.resolve(__dirname, './src/assets/scss/_global.scss')],
+    })
+}
 
 module.exports = {
+  runtimeCompiler: true,
   siteName: 'Gridsome',
-  plugins: []
+  plugins: [],  
+  chainWebpack(config) {
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+
+    types.forEach(type =>
+      addStyleResource(config.module.rule('scss').oneOf(type)),
+    )
+  },
 }
