7bfdc989bfc4ce55f00d2feefb709fb124db0825
RobertH-O <robertohernandez2610@gmail.com>
2021-10-07 17:18:31 -0400

    Segundos ajustes de portrait generales

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,23 +8,23 @@
           src="../assets/images/novanet-logo.svg"
         />
       </g-link>
-      <g-link class="nav__link" to="/nosotros/">
+      <g-link class="nav__link nav__link--mobile" to="/nosotros/">
         <div data-icon="f" class="nav__link--icon icon"></div>
         <h4 class="nav__link--title">Nosotros</h4>
       </g-link>
-      <g-link class="nav__link" to="/servicios/">
+      <g-link class="nav__link nav__link--mobile" to="/servicios/">
         <div data-icon="m" class="nav__link--icon icon"></div>
         <h4 class="nav__link--title">Servicios</h4>
       </g-link>
-      <g-link class="nav__link" to="/portafolio/">
+      <g-link class="nav__link nav__link--mobile" to="/portafolio/">
         <div data-icon="t" class="nav__link--icon icon"></div>
         <h4 class="nav__link--title">Portafolio</h4>
       </g-link>
-      <g-link class="nav__link" to="/blog/">
+      <g-link class="nav__link nav__link--mobile" to="/blog/">
         <div data-icon="q" class="nav__link--icon icon"></div>
         <h4 class="nav__link--title">Blog</h4>
       </g-link>
-      <g-link class="nav__link" to="/contacto/">
+      <g-link class="nav__link nav__link--mobile" to="/contacto/">
         <div data-icon="z" class="nav__link--icon icon"></div>
         <h4 class="nav__link--title-contacto">Contacto</h4>
       </g-link>
