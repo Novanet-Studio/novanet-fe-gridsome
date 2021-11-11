927ef394d0ccee03ad00adebd6420bfc6b88e072
Freddy Polania <fpolania@gmail.com>
2021-11-11 16:04:46 -0400

    Hamburguer menu para todos los media queries

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,37 +1,47 @@
 <template>
   <header class="header">
     <nav class="nav--main">
-      <g-link class="nav__link" to="/">
-        <img
-          class="nav__link--logo"
-          src="../assets/images/novanet-logo.svg"
-          alt="Logo de novanet"
-        />
-      </g-link>
-      <g-link class="nav__link nav__link--view" to="/nosotros/">
-        <div data-icon="f" class="nav__link--icon icon"></div>
-        <h4 class="nav__link--title">Nosotros</h4>
-      </g-link>
-      <g-link class="nav__link nav__link--view" to="/servicios/">
-        <div data-icon="m" class="nav__link--icon icon"></div>
-        <h4 class="nav__link--title">Servicios</h4>
-      </g-link>
-      <g-link class="nav__link nav__link--view" to="/portafolio/">
-        <div data-icon="t" class="nav__link--icon icon"></div>
-        <h4 class="nav__link--title">Portafolio</h4>
-      </g-link>
-      <g-link class="nav__link nav__link--view" to="/blog/">
-        <div data-icon="q" class="nav__link--icon icon"></div>
-        <h4 class="nav__link--title">Blog</h4>
-      </g-link>
-      <g-link class="nav__link nav__link--view" to="/contacto/">
-        <div data-icon="z" class="nav__link--icon icon"></div>
-        <h4 class="nav__link--title-contacto">Contacto</h4>
-      </g-link>
+      <Slide width='240'>
+        <g-link class="nav__link" to="/">
+          <img
+            class="nav__link--logo"
+            src="../assets/images/novanet-logo.svg"
+            alt="Logo de novanet"
+          />
+        </g-link>
+        <g-link class="nav__link nav__link--view" to="/nosotros/">
+          <div data-icon="f" class="nav__link--icon icon"></div>
+          <p class="nav__link--title">Nosotros</p>
+        </g-link>
+        <g-link class="nav__link nav__link--view" to="/servicios/">
+          <div data-icon="m" class="nav__link--icon icon"></div>
+          <p class="nav__link--title">Servicios</p>
+        </g-link>
+        <g-link class="nav__link nav__link--view" to="/portafolio/">
+          <div data-icon="t" class="nav__link--icon icon"></div>
+          <p class="nav__link--title">Portafolio</p>
+        </g-link>
+        <g-link class="nav__link nav__link--view" to="/blog/">
+          <div data-icon="q" class="nav__link--icon icon"></div>
+          <p class="nav__link--title">Blog</p>
+        </g-link>
+        <g-link class="nav__link nav__link--view" to="/contacto/">
+          <div data-icon="z" class="nav__link--icon icon"></div>
+          <p class="nav__link--title">Contacto</p>
+        </g-link>
+      </Slide>
     </nav>
   </header>
 </template>
 
-<script lang="scss">
-@import '../layouts/Home.scss'
+<script>
+import { Slide } from 'vue-burger-menu' // import the CSS transitions you wish to use, in this case we are using `Slide`
+
+export default {
+  components: {
+    Slide,
+  },
+}
+
+import '../layouts/Home.scss'
 </script>
